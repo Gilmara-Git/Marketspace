55a0998017ce1b852ae4114732585745acba2639
Gilmara Pimentel <gilmarapq@hotmail.com>
2023-09-04 19:21:47 -0400

    refactor: adjusted spinner color

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -4,7 +4,7 @@ import { Center, Spinner} from 'native-base';
 export const Loading =()=>{
     return (<
         Center flex={1}bg='gray.200'>
-            <Spinner color='red.400'/>
+            <Spinner color='blue.900'/>
         </Center>
     )
 };
