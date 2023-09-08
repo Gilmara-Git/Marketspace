d87393b0ab727826d6c8e213097250b07bdfdb7e
Gilmara Pimentel <gilmarapq@hotmail.com>
2023-09-08 09:07:40 -0400

    feat: add images and product list array

---
+++ src/utils/products.tsx
@@ -0,0 +1,54 @@
+import sneaker from '@assets/red_sneaker.png';
+import bicycle from '@assets/bicycle.png';
+import chest from '@assets/chest.png';
+import couch from '@assets/couch.png';
+
+
+
+export const products = [
+    {
+        id: '1',
+        name: 'Red Sneaker',
+        image: 'sneaker'
+,       description: 'Red Sneaker',
+        price: '59.90',
+        isNew: 'true'
+    },{
+        id: '2',
+        name: 'Bicycle',
+        image: 'bicycle'
+,       description: 'Bicycle',
+        price: '120.00',
+        isNew: 'true'
+    },{
+        id: '3',
+        name: 'Chest',
+        image: 'chest'
+,       description: 'Chest with 2 drawers',
+        price: '79.90',
+        isNew: 'false'
+    },{
+        id: '4',
+        name: 'Couch',
+        image: 'couch'
+,       description: 'Sofa Bed' ,
+        price: '1,200.00',
+        isNew: 'false'
+  },
+    {
+        id: '5',
+        name: 'Sample',
+        image: 'couch'
+    ,       description: 'Sofa Bed' ,
+        price: '1,200.00',
+        isNew: 'false'
+    },
+    {
+        id: '6',
+        name: 'Sample product',
+        image: 'couch'
+    ,       description: 'Sofa Bed' ,
+        price: '1,200.00',
+        isNew: 'false'
+    }
+];
