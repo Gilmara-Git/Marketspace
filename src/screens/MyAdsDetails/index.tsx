21948018e7b6055927685435ada9a41c62804239
Gilmara Pimentel <gilmarapq@hotmail.com>
2023-09-12 18:51:43 -0400

    feat: add my_product data

---
+++ src/screens/MyAdsDetails/index.tsx
@@ -0,0 +1,14 @@
+import { VStack , Heading, Text , FlatList} from 'native-base';
+
+export const MyAdsDetails = ()=>{
+    //params
+    // if isActive === false product photo is darker and replace button Deactivate Ad to Activate Ad
+    const isActive = true;
+
+
+    return (
+        <VStack>
+            <Heading>9 Ads</Heading>
+        </VStack>
+    );
+};
