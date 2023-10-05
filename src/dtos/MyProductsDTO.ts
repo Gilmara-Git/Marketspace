360925f391679a74dd17a18023d424fe86521715
Gilmara Pimentel <gilmarapq@hotmail.com>
2023-10-05 16:40:10 -0400

    feat: add MyProductsDTO

---
+++ src/dtos/MyProductsDTO.ts
@@ -0,0 +1,12 @@
+export type MyProductsDTO = {
+    id: string,
+    name: string,
+    description: string, 
+    is_new: boolean, 
+    price: string, 
+    accept_trade: boolean,
+    user_id: string,
+    is_active: boolean, 
+    payment_methods: string[], 
+    product_images: any[]
+};
